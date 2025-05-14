219ae43d4469462bbe120b975d65863d252c7c51
Jo Asplin <jo.asplin@met.no>
2025-05-14 22:23:55 +0200

    Added quality code and -vocabulary

---
+++ datastore/migrate/data/migrations/1747252021_quality_code.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE observation DROP COLUMN quality_code BIGINT;
+ALTER TABLE time_series DROP COLUMN quality_code_vocabulary TEXT;
---
+++ datastore/migrate/data/migrations/1747252021_quality_code.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE observation ADD COLUMN quality_code BIGINT;
+ALTER TABLE time_series ADD COLUMN quality_code_vocabulary TEXT NOT NULL;
