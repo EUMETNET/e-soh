95783eefdfd89fa36bd786285a092385c787981f
fjugipe <jukka.pelli@fmi.fi>
2025-05-14 12:08:26 +0300

    feat: add dynamic collections routes based on a config file

---
+++ api/openapi/collections_metadata.py
@@ -0,0 +1,11 @@
+collections_metadata = {
+    "observations":{
+        "id": "observations",
+        "title": "Meteorological Observations in Europe",
+        "license": {
+            "name": "CC-BY-4.0",
+            "url": "https://creativecommons.org/licenses/by/4.0/",
+        },
+        "crs": ["OGC:CRS84"],
+    }
+}
