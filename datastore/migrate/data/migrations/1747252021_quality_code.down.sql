db19b1caea9b68f4cfef91b207bcab634d7657ba
jo-asplin-met-no <jo.asplin@met.no>
2025-05-16 12:33:56 +0200

    Removed superfluous types
    Co-authored-by: Jeffrey-Vervoort-KNMI <80899828+Jeffrey-Vervoort-KNMI@users.noreply.github.com>

--- datastore/migrate/data/migrations/1747252021_quality_code.down.sql
+++ datastore/migrate/data/migrations/1747252021_quality_code.down.sql
@@ -1,2 +1,2 @@
-ALTER TABLE observation DROP COLUMN quality_code BIGINT;
-ALTER TABLE time_series DROP COLUMN quality_code_vocabulary TEXT;
+ALTER TABLE observation DROP COLUMN quality_code;
+ALTER TABLE time_series DROP COLUMN quality_code_vocabulary;
