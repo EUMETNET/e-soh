d4f4b1dfaa44b20a8d0916918ce034981a0e684f
Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>
2025-09-02 09:18:43 +0200

    Update ingest/api/send_mqtt.py
    Co-authored-by: Jukka Pelli <132542798+fjugipe@users.noreply.github.com>

--- ingest/api/send_mqtt.py
+++ ingest/api/send_mqtt.py
@@ -8,15 +8,12 @@ logger = logging.getLogger(__name__)
 
 if "MQTT_TOPIC_PREPEND" in os.environ:
     mqtt_topic_prepend = os.getenv("MQTT_TOPIC_PREPEND", "")
-    if mqtt_topic_prepend and not mqtt_topic_prepend.endswith("/") and len(mqtt_topic_prepend) > 1:
-        mqtt_topic_prepend = mqtt_topic_prepend + "/"
-    else:
+    if not mqtt_topic_prepend or mqtt_topic_prepend == "/":
         mqtt_topic_prepend = ""
         logger.error(
-            "MQTT_TOPIC_PREPEND cannot be just a '/' and can not be empty. Setting topic prepend to empty string."
-        )
-else:
-    mqtt_topic_prepend = ""
+            "MQTT_TOPIC_PREPEND cannot be just a '/'. Setting topic prepend to empty string."
+    else:
+        mqtt_topic_prepend = mqtt_topic_prepend if mqtt_topic_prepend.endswith("/") else mqtt_topic_prepend + "/"
 
 
 def connect_mqtt(mqtt_conf: dict):
