e667144bfe6709e153059fb1008e07b0350ed431
Amund Isaksen <amundi@met.no>
2025-10-27 14:26:14 +0100

    Add absolute path as default

--- api/openapi/openapi_metadata.py
+++ api/openapi/openapi_metadata.py
@@ -2,7 +2,8 @@ import os
 import json
 
 with open(
-    os.getenv("OPENAPI_METADATA_PATH", "./openapi_default_files/openapi_metadata.json"),
+    os.getenv("OPENAPI_METADATA_PATH", "/app/openapi_default_files/openapi_metadata.json"),
     "r",
 ) as file:
     openapi_metadata = json.load(file)
+    print(openapi_metadata)
