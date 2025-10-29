200a04e050e29bf5c4a486daf1b1e30826c9054a
istvans <istvans@met.no>
2025-10-29 16:11:32 +0100

    Update BUFR test

--- ingest/test/test_verify_json_payloads.py
+++ ingest/test/test_verify_json_payloads.py
@@ -3,9 +3,9 @@ import pytest
 from jsonschema import ValidationError
 from api.model import JsonMessageSchema
 from api.messages import build_all_json_payloads_from_bufr
-from ingest.bufr.bufresohmsg_py import init_bufr_schema_py
-from ingest.bufr.bufresohmsg_py import init_bufrtables_py
-from ingest.bufr.bufresohmsg_py import init_oscar_py
+from bufr_tools.bufresohmsg_py import init_bufr_schema_py
+from bufr_tools.bufresohmsg_py import init_bufrtables_py
+from bufr_tools.bufresohmsg_py import init_oscar_py
 
 
 @pytest.mark.timeout(1000)
@@ -13,8 +13,6 @@ from ingest.bufr.bufresohmsg_py import init_oscar_py
 def test_verify_json_payload_bufr(bufr_file_path, capsys):
 
     init_bufrtables_py("")
-    init_oscar_py("./src/ingest/bufr/oscar/oscar_stations_all.json")
-    init_bufr_schema_py("./src/ingest/schemas/bufr_to_e_soh_message.json")
 
     with open(bufr_file_path, "rb") as file:
         bufr_content = file.read()
