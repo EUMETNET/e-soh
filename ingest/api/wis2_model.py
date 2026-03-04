d264aac625c146a47ca76bb18fe37d5901d6e7f3
Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>
2026-03-04 06:53:10 +0100

    Update ingest/api/wis2_model.py
    Co-authored-by: Jukka Pelli <132542798+fjugipe@users.noreply.github.com>

--- ingest/api/wis2_model.py
+++ ingest/api/wis2_model.py
@@ -91,7 +91,7 @@ class Properties(BaseModel):
     integrity: Optional[Integrity] = Field(None, exclude_from_schema=True)
 
     @model_validator(mode="after")
-    def set_dateimte(self):
+    def set_datetime(self):
         assert bool(self.datetime) != bool(self.start_datetime and self.end_datetime), (
             "Set datetime or start_datetime and end_datetime. At least one and not both. "
             + f"{self.datetime}, {self.start_datetime} - {self.end_datetime}"
