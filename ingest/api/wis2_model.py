38cd3a4d53533d70323bb70f2bbda16d35176b35
Amund Isaksen <31344542+Teddy-1000@users.noreply.github.com>
2026-03-04 06:58:58 +0100

    Update ingest/api/wis2_model.py
    Co-authored-by: Jukka Pelli <132542798+fjugipe@users.noreply.github.com>

--- ingest/api/wis2_model.py
+++ ingest/api/wis2_model.py
@@ -58,7 +58,7 @@ class Content(BaseModel):
 
 class Integrity(BaseModel):
     method: Literal["sha256", "sha384", "sha512", "sha3-256", "sha3-384", "sha3-512"]
-    value: str = Field(..., desciption="The hash value for the value field in the message content.")
+    value: str = Field(..., description="The hash value for the value field in the message content.")
 
 
 class Properties(BaseModel):
