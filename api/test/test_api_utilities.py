bf09f7a4fe21b6bfae4c49e4d7e765ff858912c0
fjugipe <jukka.pelli@fmi.fi>
2026-03-10 09:47:14 +0100

    test: datetime expression support

--- api/test/test_api_utilities.py
+++ api/test/test_api_utilities.py
@@ -1,6 +1,9 @@
 import pytest
 
 from utilities import get_levels_values
+from utilities import get_datetime_range
+from contextlib import nullcontext as does_not_raise
+from fastapi import HTTPException
 
 levels_in_out = [
     ("1", ["100"]),
@@ -17,3 +20,41 @@ levels_in_out = [
 @pytest.mark.parametrize("levels_in, levels_out", levels_in_out)
 def test_get_levels_values(levels_in, levels_out):
     assert get_levels_values(levels_in) == levels_out
+
+
+datetime_in_out = [
+    (
+        "2026-01-01T00:00Z/2026-01-01T02:00Z",
+        False,
+    ),
+    (
+        "2026-01-01T00:00:00Z/..",
+        False,
+    ),
+    (
+        "../2026-01-01T00:00Z",
+        False,
+    ),
+    (
+        "2026-01-01T00:00:00.123Z",
+        False,
+    ),
+    (
+        "2026-01-01T00:00+02:00/2026-01-05T00:00-02:00",
+        False,
+    ),
+    (
+        "2026-01-01/2026-01-02",
+        True,
+    ),
+    (
+        "2026-01-01T00:00Z/2025-12-31T23:59Z",
+        True,
+    ),
+]
+
+
+@pytest.mark.parametrize("datetime_in, raise_error", datetime_in_out)
+def test_get_datetime_range(datetime_in, raise_error):
+    with pytest.raises(HTTPException) if raise_error else does_not_raise():
+        get_datetime_range(datetime_in)
