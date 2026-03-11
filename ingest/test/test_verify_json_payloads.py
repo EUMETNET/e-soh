6ef824cb0b226d43afdec755494ded4486d2507c
Amund Isaksen <amundi@met.no>
2026-03-11 11:08:41 +0100

    Fix package dependencies

--- ingest/test/test_verify_json_payloads.py
+++ ingest/test/test_verify_json_payloads.py
@@ -1,6 +1,6 @@
 import glob
 import pytest
-from jsonschema import ValidationError
+from pydantic import ValidationError
 from api.model import JsonMessageSchema
 from api.messages import build_all_json_payloads_from_bufr
 from bufr_tools.bufresohmsg_py import init_bufrtables_py
@@ -9,7 +9,6 @@ from bufr_tools.bufresohmsg_py import init_bufrtables_py
 @pytest.mark.timeout(1000)
 @pytest.mark.parametrize("bufr_file_path", glob.glob("test/test_data/bufr/*.buf*"))
 def test_verify_json_payload_bufr(bufr_file_path, capsys):
-
     init_bufrtables_py("")
 
     with open(bufr_file_path, "rb") as file:
