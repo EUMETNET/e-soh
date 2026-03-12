6e71691c8835f4d3e6b14c0021f01ed009edc7ae
fjugipe <jukka.pelli@fmi.fi>
2026-03-12 13:46:42 +0100

    test: get_datetime_range against known returned timestamps

--- api/test/test_api_utilities.py
+++ api/test/test_api_utilities.py
@@ -1,4 +1,5 @@
 import pytest
+from google.protobuf.timestamp_pb2 import Timestamp
 
 from utilities import get_levels_values
 from utilities import get_datetime_range
@@ -25,36 +26,58 @@ def test_get_levels_values(levels_in, levels_out):
 datetime_in_out = [
     (
         "2026-01-01T00:00Z/2026-01-01T02:00Z",
+        (
+            Timestamp(seconds=1767225600),
+            Timestamp(seconds=1767232801),
+        ),
         False,
     ),
     (
         "2026-01-01T00:00:00Z/..",
+        (
+            Timestamp(seconds=1767225600),
+            Timestamp(seconds=253402300799, nanos=999999000),
+        ),
         False,
     ),
     (
         "../2026-01-01T00:00Z",
+        (
+            Timestamp(seconds=-62135596800),
+            Timestamp(seconds=1767225601),
+        ),
         False,
     ),
     (
         "2026-01-01T00:00:00.123Z",
+        (
+            Timestamp(seconds=1767225600, nanos=123000000),
+            Timestamp(seconds=1767225601, nanos=123000000),
+        ),
         False,
     ),
     (
         "2026-01-01T00:00+02:00/2026-01-05T00:00-02:00",
+        (
+            Timestamp(seconds=1767218400),
+            Timestamp(seconds=1767578401),
+        ),
         False,
     ),
     (
         "2026-01-01/2026-01-02",
+        None,
         True,
     ),
     (
         "2026-01-01T00:00Z/2025-12-31T23:59Z",
+        None,
         True,
     ),
 ]
 
 
-@pytest.mark.parametrize("datetime_in, raise_error", datetime_in_out)
-def test_get_datetime_range(datetime_in, raise_error):
+@pytest.mark.parametrize("datetime_in, timestamps_out, raise_error", datetime_in_out)
+def test_get_datetime_range(datetime_in, timestamps_out, raise_error):
     with pytest.raises(HTTPException) if raise_error else does_not_raise():
-        get_datetime_range(datetime_in)
+        assert get_datetime_range(datetime_in) == timestamps_out
