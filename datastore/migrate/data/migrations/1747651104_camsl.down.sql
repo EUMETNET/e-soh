13365a08ebdde1ecd5fedd5d284d4877074ca7a6
Jo Asplin <jo.asplin@met.no>
2026-03-12 12:26:09 +0100

    Added better/safer migrations for moving camsl to geo_point table

--- datastore/migrate/data/migrations/1747651104_camsl.down.sql
+++ datastore/migrate/data/migrations/1747651104_camsl.down.sql
@@ -1,3 +1,1 @@
-ALTER TABLE geo_point DROP CONSTRAINT geo_point_point_camsl_key;
-ALTER TABLE geo_point ADD CONSTRAINT geo_point_point_key UNIQUE (point);
-ALTER TABLE geo_point DROP COLUMN camsl;
+ALTER TABLE observation DROP COLUMN camsl;
---
+++ datastore/migrate/data/migrations/1773314184_move_camsl_to_geo_point.down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE geo_point DROP CONSTRAINT geo_point_point_camsl_key;
+ALTER TABLE geo_point ADD CONSTRAINT geo_point_point_key UNIQUE (point);
+ALTER TABLE geo_point DROP COLUMN camsl;
+ALTER TABLE observation ADD COLUMN camsl BIGINT;
